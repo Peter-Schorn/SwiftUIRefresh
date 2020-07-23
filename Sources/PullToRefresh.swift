31279e35a3d614b433ca1fdd32c16cb8de290942
Peter-Schorn <petervschorn@gmail.com>
2020-07-23 17:13:56 -0500

    Added support for scroll views.

--- Sources/PullToRefresh.swift
+++ Sources/PullToRefresh.swift
@@ -1,34 +1,41 @@
 import SwiftUI
 import Introspect
 
+public enum ViewType {
+    case scrollView, list
+}
+
 private struct PullToRefresh: UIViewRepresentable {
     
-    @Binding var isShowing: Bool
+    @Binding var isRefreshing: Bool
     let onRefresh: () -> Void
+    let viewType: ViewType
     
     public init(
-        isShowing: Binding<Bool>,
+        isRefreshing: Binding<Bool>,
+        viewType: ViewType,
         onRefresh: @escaping () -> Void
     ) {
-        _isShowing = isShowing
+        self._isRefreshing = isRefreshing
+        self.viewType = viewType
         self.onRefresh = onRefresh
     }
     
     public class Coordinator {
         let onRefresh: () -> Void
-        let isShowing: Binding<Bool>
+        let isRefreshing: Binding<Bool>
         
         init(
             onRefresh: @escaping () -> Void,
-            isShowing: Binding<Bool>
+            isRefreshing: Binding<Bool>
         ) {
             self.onRefresh = onRefresh
-            self.isShowing = isShowing
+            self.isRefreshing = isRefreshing
         }
         
         @objc
         func onValueChanged() {
-            isShowing.wrappedValue = true
+            isRefreshing.wrappedValue = true
             onRefresh()
         }
     }
@@ -40,10 +47,13 @@ private struct PullToRefresh: UIViewRepresentable {
         return view
     }
     
+    // MARK: - Table View -
     private func tableView(entry: UIView) -> UITableView? {
         
         // Search in ancestors
-        if let tableView = Introspect.findAncestor(ofType: UITableView.self, from: entry) {
+        if let tableView = Introspect.findAncestor(
+            ofType: UITableView.self, from: entry
+        ) {
             return tableView
         }
 
@@ -52,42 +62,107 @@ private struct PullToRefresh: UIViewRepresentable {
         }
 
         // Search in siblings
-        return Introspect.previousSibling(containing: UITableView.self, from: viewHost)
+        return Introspect.previousSibling(
+            containing: UITableView.self, from: viewHost
+        )
     }
 
-    public func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PullToRefresh>) {
+    
+    // MARK: - Scroll View -
+    private func scrollView(entry: UIView) -> UIScrollView? {
         
-        DispatchQueue.main.asyncAfter(deadline: .now()) {
+        if let scrollView = Introspect.findAncestor(
+            ofType: UIScrollView.self, from: entry
+        ) {
+        
+            return scrollView
+        }
+        
+        guard let viewHost = Introspect.findViewHost(from: entry) else {
+            return nil
+        }
+        
+        return Introspect.previousSibling(
+            containing: UIScrollView.self, from: viewHost
+        )
+        
+    }
+
+    
+    // MARK: - Update UI View
+    public func updateUIView(
+        _ uiView: UIView,
+        context: UIViewRepresentableContext<PullToRefresh>
+    ) {
+        
+        DispatchQueue.main.async  {
             
-            guard let tableView = self.tableView(entry: uiView) else {
+            guard let scrollableView = self.scrollView(entry: uiView) else {
                 return
             }
             
-            if let refreshControl = tableView.refreshControl {
-                if self.isShowing {
+            // if the refresh control has already been attached,
+            // then update it based on the `isRefreshing` binding
+            if let refreshControl = scrollableView.refreshControl {
+                if self.isRefreshing {
                     refreshControl.beginRefreshing()
                 } else {
                     refreshControl.endRefreshing()
                 }
-                return
+                return  // note the return
             }
             
+            // else, create a new refresh control
             let refreshControl = UIRefreshControl()
-            refreshControl.addTarget(context.coordinator, action: #selector(Coordinator.onValueChanged), for: .valueChanged)
-            tableView.refreshControl = refreshControl
+            
+            // add a delegate to watch for when the user scrolls
+            refreshControl.addTarget(
+                context.coordinator,
+                action: #selector(Coordinator.onValueChanged),
+                for: .valueChanged
+            )
+            
+            // attach the refresh control to the scrollable view
+            scrollableView.refreshControl = refreshControl
         }
     }
     
     public func makeCoordinator() -> Coordinator {
-        return Coordinator(onRefresh: onRefresh, isShowing: $isShowing)
+        return Coordinator(
+            onRefresh: onRefresh, isRefreshing: $isRefreshing
+        )
     }
 }
 
 extension View {
-    public func pullToRefresh(isShowing: Binding<Bool>, onRefresh: @escaping () -> Void) -> some View {
+    
+    /**
+    Attaches a refresh control to the view.
+    
+    - Parameters:
+      - isRefreshing: True if view is currently refreshing,
+            else false.
+      - viewType: Either `.scrollView` or `.list`.
+      - onRefresh: A closure that gets called
+            when the user pulls down to refresh the view.
+            When this happens, `isRefreshing` gets set to true.
+            You must set it back to false when new data
+            has finished loading, which will dismiss
+            the activity indicator.
+    */
+    public func pullToRefresh(
+        isRefreshing: Binding<Bool>,
+        viewType: ViewType,
+        onRefresh: @escaping () -> Void
+    ) -> some View {
+        
         return overlay(
-            PullToRefresh(isShowing: isShowing, onRefresh: onRefresh)
-                .frame(width: 0, height: 0)
+            PullToRefresh(
+                isRefreshing: isRefreshing,
+                viewType: viewType,
+                onRefresh: onRefresh
+            )
+            .frame(width: 0, height: 0)
         )
     }
 }
